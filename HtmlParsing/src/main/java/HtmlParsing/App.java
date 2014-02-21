f259558753ee265f817579413044c0debb7bfe5a
ipolding <ipolding@gmail.com>
2014-02-21 19:24:47 +0000

    Working csv generator but need to fix odd numbered columns bug

--- HtmlParsing/src/main/java/HtmlParsing/App.java
+++ HtmlParsing/src/main/java/HtmlParsing/App.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.Random;
 
 import java.io.File;
 import java.io.IOException;
@@ -23,7 +24,7 @@ public class App
     public static void main( String[] args ) throws IOException {
         Document doc = null;
 
-        File input = new File("C:/tutorial/javaTutorials/HtmlParsing/src/main/resources/tmp/html.html");
+        File input = new File("C:/github/javaPortfolio/HtmlParsing/src/main/resources/tmp/html.html");
         doc = Jsoup.parse(input, "UTF-8");
 
         Elements html_novels = doc.getElementsByTag("a");
@@ -35,30 +36,38 @@ public class App
                     System.out.println(element.text());
                     novels.add(element.text());
             }
+       }
 
-        }
+// TODO fix this so that it works for odd number column headers
+       String[] columnHeaders = {"Title","Author"};
+       generateCsv(novels,columnHeaders);
+
     }
 
-    private void generateCsv(List strings, String[] columnHeaders){
+    private static void generateCsv(List strings, String[] columnHeaders){
         FileWriter fileWriter = null;
+        Random testGenerator = new Random();
         try {
-            fileWriter = new FileWriter("C:/tutorial/javaTutorials/HtmlParsing/src/main/resources/tmp/output.txt");
+            fileWriter = new FileWriter("C:/github/javaPortfolio/HtmlParsing/src/main/resources/tmp/Example_output" + testGenerator.nextInt(999) + ".csv" );
 
             for (String header: columnHeaders){
                 fileWriter.append(header);
+                fileWriter.append(", ");
             }
             fileWriter.append("\n");
 
+            int listPosition = 0;
             for (int i = 0; i < strings.size(); i += columnHeaders.length) {
-                int x = 0;
-                while (x < columnHeaders.length)
-                {fileWriter.append((String)strings.get(x));
-                    x++;
+                for (int j = 0; j < columnHeaders.length; j++)
+                {fileWriter.append((String) strings.get(listPosition));
+                 fileWriter.append(", ");
+                    listPosition++;
                 }
                 fileWriter.append("\n");
             }
 
-
+            fileWriter.flush();
+            fileWriter.close();
         } catch (IOException ex)
         {ex.printStackTrace();}
 
